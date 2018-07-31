87f0aee4391c89fee68e5460929c4af00703cb38
Your Name <you@example.com>
2018-07-31 12:29:41 +0800

    tea joke

--- ariel_bee/ariel_bee/ViewController.swift
+++ ariel_bee/ariel_bee/ViewController.swift
@@ -1,43 +1,133 @@
 //
+
 //  ViewController.swift
+
 //  ariel_bee
+
 //
+
 //  Created by sweetiewill on 7/26/18.
+
 //  Copyright © 2018 axiom6ai. All rights reserved.
+
 //
+
+
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
         for x in 0 ... 6 {
+            
             if (x == 6){
+                
                 print ("Current Value is:  ", x)
                 
-                usbImage.isHidden = false
-                questionImage.isHidden = true
+                
+                
+                teapot.isHidden = true
+                
+                question.isHidden = false
+                
                 print ("Show picture!")
                 
+                
+                
             }
+                
             else{
+                
                 print ("Current Value is:  ", x)
+                
             }
+            
         }
-
-
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBOutlet weak var questionImage: UIImageView!
     
-    @IBOutlet weak var usbImage: UIImageView!
-
+    
+    
+    
+    
+    
+    @IBAction func Answer(_ sender: Any) {
+    
+    
+        if (question.isHidden == false){
+            
+            revealAnswer()
+            
+            
+            
+        }
+            
+        else if (teapot.isHidden == false){
+            
+            hideQuestion()
+            
+        }
+        
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+        
+        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    
+    
+    func revealAnswer(){
+        
+        teapot.isHidden = false
+        
+        question.isHidden = true
+        
+    }
+    
+    
+    
+    func hideQuestion(){
+        
+        teapot.isHidden = true
+        
+        question.isHidden = false
+        
+    }
+    
+    
+    
+    
+    
+   
+    @IBOutlet weak var question: UIImageView!
+    
+    
+    
+    
+    @IBOutlet weak var teapot: UIImageView!
+    
+    
+    
+    
+    
+    
+    
 }
+
 
